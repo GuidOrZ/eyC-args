2ffaaea44e6372ddb7c2e2f0e6308c07007925f7
GuidOrZ <guidozmail@gmail.com>
2026-03-30 19:02:28 +0200

    refactor: restructure argument parsing logic
    Added support for ARGTYPE_OPTION.
    Separate flag handling into _assigning_flag().

--- examples/basic_main.c
+++ examples/basic_main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //Include the header
 #include "../eyc_args.h"
 
 int main (int argc, char *argv[]) {
-    int pos1;
+    const char* pos1;
     int pos2;
     int help;
+    const char* path;
 
     //Use this macro to define the arglist.
     DEFINE_ARGS(
-        ARGS_POSITIONAL(DATATYPE_INT, &pos1),
+        ARGS_POSITIONAL(DATATYPE_STRING, &pos1),
         ARGS_POSITIONAL(DATATYPE_INT, &pos2),
+        ARGS_OPTION("-p", "-path", DATATYPE_STRING, &path),
         ARGS_FLAG("-h", "-help", &help)
     );
 
     parse_args(argc, argv, arglist);
 
-    printf("%d\n%d\n%d\n", pos1, pos2, help);
+    printf("POS1: %s\nPOS2: %d\nHELP: %d\nPATH: %s\n", pos1, pos2, help, path);
 
     return 0;
 }
--- eyc_args.h
+++ eyc_args.h
@@ -4,14 +4,14 @@
 #ifndef PARSE_H
 #define PARSE_H
 
-/* MACROS FOR THE LIST DEFINITION*/
+/* MACROS FOR THE LIST DEFINITION */
 
 #define ARGS_END {NULL, NULL, NULL, ARGTYPE_POSITIONAL, DATATYPE_END}
 
 #define ARGS_POSITIONAL(type, data) \
     {NULL, NULL, (void*)(data), ARGTYPE_POSITIONAL, (type)}
 
-#define ARGS_OPTION(short_f, long_f, data, type) \
+#define ARGS_OPTION(short_f, long_f, type, data) \
     {(short_f), (long_f), (void*)(data), ARGTYPE_OPTION, (type)}
 
 #define ARGS_FLAG(short_f, long_f, data) \
@@ -49,12 +49,10 @@ void _init_value (args_type arglist []) {
     for (int i = 0; arglist[i].data_type != DATATYPE_END; i++) {
         switch (arglist[i].data_type)
         {
-            case DATATYPE_INT:    {int *r = (int*)arglist[i].data;      *r = 0;     break;
-            }
-            case DATATYPE_FLOAT:  {float *r = (float*)arglist[i].data;  *r = 0.0f;  break;
-            }
-            case DATATYPE_STRING: {char **r = (char**)arglist[i].data;  *r = NULL;  break;
-            }
+            case DATATYPE_INT:    {int *r = (int*)arglist[i].data;      *r = 0;     break; }
+            case DATATYPE_FLOAT:  {float *r = (float*)arglist[i].data;  *r = 0.0f;  break; }
+            case DATATYPE_STRING: {char **r = (char**)arglist[i].data;  *r = NULL;  break; }
+            case DATATYPE_END:                                                                      break;
         }
     }
 }
@@ -65,6 +63,16 @@ void _assign_value (args_type *arg, const char *str) {
         case DATATYPE_INT:    *(int *)arg->data         = atoi(str);                break;
         case DATATYPE_FLOAT:  *(float *)arg->data       = (float)atof(str);         break;
         case DATATYPE_STRING: *(const char **)arg->data = str;                      break;
+        case DATATYPE_END:                                                          break;
+    }
+}
+
+void _assigning_flag(args_type *arg, const char *str) {
+    if (arg->flag && strcmp(arg->flag, str) == 0) {
+        *(int *)arg->data = 1;
+    }
+    else if (arg->verbose_flag && strcmp(arg->verbose_flag, str) == 0) {
+        *(int *)arg->data = 2;
     }
 }
 
@@ -76,22 +84,24 @@ int parse_args(int argc, char *argv[], args_type arglist []) {
     int index = 0;
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
-            //OR A FLAG OR AN OPTION
-            //TODO:Flags and Options
             for (int j = 0; arglist[j].data_type != DATATYPE_END; j++) {
-                if (arglist[j].flag) {
-                    if (strcmp(arglist[j].flag, argv[i]) == 0) {
-                        int *r = (int*)arglist[j].data;
-                        *r = 1; //SHORT
-                    }
-                    if (strcmp(arglist[j].verbose_flag, argv[i]) == 0) {
-                        int *r = (int*)arglist[j].data;
-                        *r = 2; //VERBOSE
+                //FLAG
+                if (arglist[j].arg_type == ARGTYPE_FLAG) {
+                    _assigning_flag(&arglist[j], argv[i]);
+                }
+                //OPTION
+                if (arglist[j].arg_type == ARGTYPE_OPTION) {
+                    if ((arglist[j].flag && strcmp(arglist[j].flag, argv[i]) == 0) ||
+                        (arglist[j].verbose_flag && strcmp(arglist[j].verbose_flag, argv[i]) == 0)) {
+                        if (i + 1 < argc && argv[i+1][0] != '-') {
+                            _assign_value(&arglist[j], argv[i+1]);
+                            i++;
+                        }
                     }
                 }
             }
         }
-        else {
+        else {  //POSITIONAL
             for (; arglist[index].data_type != DATATYPE_END; index ++) {
                 if (arglist[index].arg_type == ARGTYPE_POSITIONAL) {
                     _assign_value(&arglist[index], argv[i]);
